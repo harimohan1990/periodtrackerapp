8562c79ebe6f99ccb28b3afe36ed9ad1b90a2a51
hari mohan <harimohan@haris-MacBook-Air-2.local>
2025-04-16 20:25:25 +0530

    made with love

--- src/App.tsx
+++ src/App.tsx
@@ -6,16 +6,24 @@ import './App.css';
 const App: React.FC = () => {
   return (
     <div className="container">
-    <h1 className='heading'>Period Menstruation Tracker</h1>
-    <p className="intro">
-      🌸 Understanding Menstruation: A Natural Part of Life<br/>
-      Menstruation, or a period, is a normal and healthy part of the reproductive cycle. It happens when the uterus sheds its lining if pregnancy doesn’t occur.<br/><br/>
-      Cycles usually last 21–35 days, with bleeding for 3–7 days. Common symptoms include cramps, mood changes, bloating, and fatigue.<br/><br/>
-      Periods are nothing to be ashamed of — they’re a natural sign of health. Tracking your cycle helps you stay in tune with your body and supports overall well-being.
-    </p>
-    <PeriodForm />
-    <PeriodList />
-  </div>
+      <h1 className="heading">Period Menstruation Tracker</h1>
+      
+      <p className="intro">
+        🌸 <strong>Understanding Menstruation:</strong> A Natural Part of Life<br />
+        Menstruation, or a period, is a normal and healthy part of the reproductive cycle. It happens when the uterus sheds its lining if pregnancy doesn’t occur.<br /><br />
+        Cycles usually last 21–35 days, with bleeding for 3–7 days. Common symptoms include cramps, mood changes, bloating, and fatigue.<br /><br />
+        Periods are nothing to be ashamed of — they’re a natural sign of health. Tracking your cycle helps you stay in tune with your body and supports overall well-being.
+      </p>
+
+      <PeriodForm />
+      <PeriodList />
+
+      <footer className="footer">
+        <p>
+          Made by <a href="https://www.linkedin.com/in/hari-mohan-prajapat-47299b54/" target="_blank" rel="noopener noreferrer">Hari</a>
+        </p>
+      </footer>
+    </div>
   );
 };
 
